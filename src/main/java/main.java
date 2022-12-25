015241e795c6ffec59c167b42a23678389218b43
lenaantona <mihaleva_alena@mail.ru>
2022-12-25 23:00:24 +0500

    задание семинара 2

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,65 +1,78 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.LinkedList;
+import java.util.Scanner;
 
 public class main {
 
 
-    public static float division(int a, int b){//исключение деления на 0
-        try {
-            return a/b;
-        }catch (Exception e){
-            System.out.println("Призошло исключение деление на 0");
-        }
-        return 0;
-    }
-    public static void arrayNew(int[] arr){//выход за пределы массива
-        try {
-            if (arr.length > 2) throw new Exception("массив должен быть меньше 3");
-            for (int i = 0; i < 3; i++){
-                arr[i] = i;
+    public static void inputFloat() {//ввод вещественного числа задание №1
+        Scanner scanner = new Scanner(System.in);
+        float num = 0;
+        while (true) {
+            System.out.println("Введите вещественное число: ");
+            try {
+                num = Float.parseFloat(scanner.nextLine());
+                System.out.println(num);
+                break;
+            } catch (Exception e) {
+                System.out.println("это не вещественное число, введите вещественное число");
             }
+        }
+    }
+// задание №2
+    public static void division(double [] intArray) {
+
+        try {
+            int d = 0;
+            if (d==0) throw new Exception("произошло деление на 0");
+            else {double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);}
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println("Catching exception: " + e.getMessage());
         }
     }
-    public static void readFromFile(String name) {//не существует файл
-        try (BufferedReader reader = new BufferedReader(new FileReader(name))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line + "\n");
+    public static void inputString() {//ввод ненулевой строки задание №4(пробел не считается нулевой строкой)
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Введите строку: ");
+            try {
+                String s = scanner.nextLine();
+                if (s.length()==0) throw new Exception("пустая строка, повторите ввод: ");
+                else {
+                    System.out.println("вы ввели - " + s);
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
-        } catch (Exception e){
-            System.out.println("Файл не найден");
         }
     }
-
-    public static void arraySub(int [] arr1, int [] arr2) {
-        int[] arr = new int[arr1.length];
-        try {
-            if (arr1.length != arr2.length) throw new Exception("Массивы не равны, операция недоступна");
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = arr1[i] - arr2[i];
-                System.out.print(arr[i]);
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+    public static void printSum(Integer a, Integer b) { //задание № 3
+        System.out.println(a + b);
     }
 
-    public static void main(String[] args) {
 
-//        float result = division(10,0);//деление на 0
-//        System.out.println(result);
+
+    public static void main(String[] args) throws Exception{
+//        //inputFloat(); //ввод вещественного числа задание №1
 //
-//        int [] arr = new int [3];
-//        arrayNew(arr); //за пределы массива
+//        //double [] arr = {1, 2, 3, 4, 5, 8, 6, 8, 5, 10};
+//        //division(arr); //задание №2
+//
+//        inputString();//ввод не нулевой строки задание №4
 
- //       readFromFile("pom1.xml"); //нет файла
-
-          int [] arr1 = {5, 6, 4};
-          int [] arr2 = {5, 6, 4, 4};
-          arraySub(arr1, arr2);
+        try { //задание №3
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2 };
+            abc[3] = 9;
+        } catch (ArithmeticException ex) {
+            System.out.println("Произошло деление на 0!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        }
 
     }
 
